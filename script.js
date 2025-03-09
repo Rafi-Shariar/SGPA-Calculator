a1bec1ac6fc767bac53a88c9ac60f974973816c8
Rafi <rafi.shariar619@gmail.com>
2025-03-09 19:00:06 +0600

    backend 2

--- script.js
+++ script.js
@@ -14,9 +14,7 @@ document.getElementById(`LabRadio`).addEventListener('click',function(){
 
 
 
-const Presentation = parseFloat(document.getElementById(`Presentation`).value);
-const Quiz = parseFloat(document.getElementById(`Quiz`).value);
-const Mid = parseFloat(document.getElementById(`Mid`).value);
+
 
 const LabAttendence = parseFloat(document.getElementById(`LabAttendence`).value);
 const LabPer = parseFloat(document.getElementById(`LabPer`).value);
@@ -28,18 +26,64 @@ document.getElementById(`GradCalBtn`).addEventListener(`click`,function(){
 
     const cur = document.querySelector(`.hidden`).id;
     
-    console.log(cur);
     
     if(cur != "infoTheory"){
 
         let Attendance = parseFloat(document.getElementById(`Attendance`).value);
-        Attendance = (Attendance * 7)/100;
-        console.log(Attendance);
-
         let Assignments = parseFloat(document.getElementById(`Assignments`).value);
+        let Presentation = parseFloat(document.getElementById(`Presentation`).value);
+        let Quiz = parseFloat(document.getElementById(`Quiz`).value);
+        let Mid = parseFloat(document.getElementById(`Mid`).value);
+
+        if(Attendance >100 || Assignments > 5 || Presentation >8 || Quiz > 15 || Mid > 25){
+            alert("invalid Input, Fill up Carefully");
+        }
+
+        Attendance = (7 * Attendance) / 100;
+
+        let totalMarks = Attendance + Assignments + Presentation + Quiz + Mid;
+
+        // 4.00
+        const ap = document.getElementById(`G4.00`);
+        if(80 - totalMarks > 0) ap.innerText = 80 - totalMarks;
+
+        //3.75
+        const a = document.getElementById(`G3.75`);
+        if(75 - totalMarks > 0) a.innerText = 75 - totalMarks;
+
+        //3.50
+        const am = document.getElementById(`G3.50`);
+        if(70 - totalMarks > 0) am.innerText = 70 - totalMarks;
+
+        //3.25
+        const bp = document.getElementById(`G3.25`);
+        if(65 - totalMarks > 0) bp.innerText = 65 - totalMarks;
+
+        //3.00
+        const b = document.getElementById(`G3.00`);
+        if(60 - totalMarks > 0) b.innerText = 60 - totalMarks;
+
+        //2.75
+        const bm = document.getElementById(`G2.75`);
+        if(55 - totalMarks > 0) bm.innerText = 55 - totalMarks;
+
+        //2.50
+        const cp = document.getElementById(`G2.50`);
+        if(50 - totalMarks > 0) cp.innerText = 50 - totalMarks;
+
+        //2.25
+        const c = document.getElementById(`G2.25`);
+        if(45 - totalMarks > 0) c.innerText = 45 - totalMarks;
+
+        //2.00
+        const cm = document.getElementById(`G2.00`);
+        if(40 - totalMarks > 0) b.innerText = 40 - totalMarks;
+
+
+        document.getElementById(`Ani1`).classList.add(`hidden`);
+        document.getElementById(`gradeSheet`).classList.remove(`hidden`);
+
         
-
-
         
         
         
