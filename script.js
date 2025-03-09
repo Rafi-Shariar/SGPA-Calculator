d08eff0eb63b1e58d607a44f365d956ba4448030
Rafi <rafi.shariar619@gmail.com>
2025-03-09 16:55:39 +0600

    backend started

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const Attendance = parseFloat(document.getElementById(`Attendance`).value);
-const Assignments = parseFloat(document.getElementById(`Assignments`).value);
+
+// Toggling Course Type
+document.getElementById(`theoryRadio`).addEventListener('click',function(){
+    document.getElementById(`infoTheory`).classList.remove(`hidden`);
+    document.getElementById(`infoLab`).classList.add(`hidden`);
+});
+
+document.getElementById(`LabRadio`).addEventListener('click',function(){
+    document.getElementById(`infoTheory`).classList.add(`hidden`);
+    document.getElementById(`infoLab`).classList.remove(`hidden`);
+
+});
+
+
+
+
 const Presentation = parseFloat(document.getElementById(`Presentation`).value);
 const Quiz = parseFloat(document.getElementById(`Quiz`).value);
 const Mid = parseFloat(document.getElementById(`Mid`).value);
@@ -10,23 +24,29 @@ const LabRep = parseFloat(document.getElementById(`LabRep`).value);
 const Project = parseFloat(document.getElementById(`Project`).value);
 
 
+document.getElementById(`GradCalBtn`).addEventListener(`click`,function(){
+
+    const cur = document.querySelector(`.hidden`).id;
+    
+    console.log(cur);
+    
+    if(cur != "infoTheory"){
+
+        let Attendance = parseFloat(document.getElementById(`Attendance`).value);
+        Attendance = (Attendance * 7)/100;
+        console.log(Attendance);
+
+        let Assignments = parseFloat(document.getElementById(`Assignments`).value);
+        
 
 
+        
+        
+        
+    }
+    
+})
 
-// Toggling Course Type
-document.getElementById(`theoryRadio`).addEventListener('click',function(){
-    document.getElementById(`infoTheory`).classList.remove(`hidden`);
-    document.getElementById(`infoLab`).classList.add(`hidden`);
-
-
-});
-
-
-document.getElementById(`LabRadio`).addEventListener('click',function(){
-    document.getElementById(`infoTheory`).classList.add(`hidden`);
-    document.getElementById(`infoLab`).classList.remove(`hidden`);
-
-});
 
 
 
